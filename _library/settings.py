643e1cecbf9c6a6880298dde0fe3800b1507948b
Rafael Quadros <contatorafaelquadros@gmail.com>
2023-03-06 15:02:23 -0300

    fix: default and .env

--- _library/settings.py
+++ _library/settings.py
@@ -94,13 +94,14 @@ REST_FRAMEWORK = {
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": str(os.getenv("DB_NAME")),
-        "USER": str(os.getenv("USERNAME")),
-        "PASSWORD": str(os.getenv("PASSWORD")),
-        "HOST": "localhost",
-        "PORT": os.getenv("PORT"),
+        "NAME": os.getenv("POSTGRES_DB_NAME"),
+        "USER": os.getenv("POSTGRES_USERNAME"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_DB_HOST"),
+        "PORT": os.getenv("POSTGRES_DB_PORT"),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -119,7 +120,6 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-
 
 
 # Internationalization
