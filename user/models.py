09a8857e2e8013c943bfabbd89ad0d1cdfe7fe2f
Luiza Barros <thecoderana@gmail.com>
2023-03-07 09:33:27 -0300

    feat: add create user and login

--- user/models.py
+++ user/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 import uuid
 
+
 class User(AbstractUser):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     email = models.EmailField(max_length=127, unique=True)
-    password = models.CharField(max_length=60)
-    username = models.CharField(max_length=60, unique=True)
+    password = models.CharField(max_length=127)
+    username = models.CharField(max_length=127, unique=True)
     age = models.IntegerField()
     blocked = models.BooleanField(default=False)
     is_colaborator = models.BooleanField(default=False)
