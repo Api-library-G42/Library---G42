18bf3270d2492881fb310995cc560cab4742181f
Vinicius Quirino <106350676+ViniciusQuirino@users.noreply.github.com>
2023-03-09 15:37:19 -0300

    debug false prod

--- _library/urls.py
+++ _library/urls.py
@@ -15,6 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -22,4 +28,7 @@ urlpatterns = [
     path("api/", include("book.urls")),
     path("api/", include("copies.urls")),
     path("api/", include("rented.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/swagger/", SpectacularSwaggerView.as_view(url_name="schema")),
+    path("api/docs/redoc/", SpectacularRedocView.as_view(url_name="schema")),
 ]
